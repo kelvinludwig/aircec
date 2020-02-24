86978f00585686a2159d2fa4f6c7642e91891a1c
kelvinludwig <kelvinludwig@roadseg.com.br>
2020-02-24 16:09:36 -0300

    Backend changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -12,8 +14,13 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-m1c2d.mongodb.net/a
 //req.params => acessa route params para edição ou delete (PUT, DELETE)
 //req.body = acessa corpo da requisiçao (POST)
 
-app.use(express.json());
 
+//app.use(cors({ origin: 'http://localhost:3333'}));
+
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
  
 app.listen(3333);
